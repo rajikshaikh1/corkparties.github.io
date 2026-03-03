72ec4ba782eff12295458cc0ef7f1ac404ca9ee2
Rajik Shaikh <rajikshaikh1357@gmail.com>
2026-03-03 10:57:01 +0530

    Initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,15 +16,18 @@
         });
 
 
-
-        $(document).ready(function(){
+$(document).ready(function(){
   $(".service-carousel").owlCarousel({
     loop: true,
     margin: 20,
-    nav: false,
+    nav: true,
     dots: true,
     autoplay: true,
     autoplayTimeout: 3000,
+    navText: [
+      '<i class="fa-solid fa-chevron-left"></i>',
+      '<i class="fa-solid fa-chevron-right"></i>'
+    ],
     responsive:{
         0:{ items:1 },
         768:{ items:3 },
